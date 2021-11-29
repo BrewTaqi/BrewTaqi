c355ec5acfd8dc83e3f90982af899421a9d1c345
BrewTaqi <abdullahtaqimahfuz@gmail.com>
2021-11-29 23:40:00 +0700

    Menambahkan view.php

---
+++ expor_peper-laravel/resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exportir Pepper Bangka !</title>
+</head>
+<body>
+<h1>Halaman About</h1>
+</body>
+</html>
---
+++ expor_peper-laravel/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exportir Pepper Bangka !</title>
+</head>
+<body>
+<h1>Halaman Home</h1>
+</body>
+</html>
