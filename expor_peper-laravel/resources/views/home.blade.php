b4453b0c2ac95e8437021ad4fad7bdfdf8c182bc
BrewTaqi <abdullahtaqimahfuz@gmail.com>
2021-12-11 08:55:58 +0700

    Mengubah Views

--- expor_peper-laravel/resources/views/home.blade.php
+++ expor_peper-laravel/resources/views/home.blade.php
@@ -14,7 +14,7 @@
   {{-- Navbar --}}
 <body>
   
-<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: rgb(59, 80, 69)">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color:  #3b5045">
     <div class="container">
         <a class="navbar-brand page-scroll" href="#home">Muntok White Pepper</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -56,7 +56,7 @@
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img src="gambar/Lada.jpg" class="col-block" alt="...">
-          <div class="card-img-overlay col-none col-sm-block text-center ">
+          <div class="card-img-overlay col-none col-md-block text-center ">
             <div class="text0">
             <h1 class="display-3">Bangka White Pepper</h1>
             </div>
@@ -67,7 +67,7 @@
         </div>
             <div class="carousel-item">
               <img src="gambar/lada24.jpg" class="col-block" alt="...">
-              <div class="card-img-overlay col-none col-sm-block">
+              <div class="card-img-overlay col-none col-md-block">
                   <div class="text1">
                     <ul class="list-unstyled">
                       <li>We supplied the authentic Muntok white pepper from Bangka Belitung,</li>
@@ -78,7 +78,7 @@
             </div>
               <div class="carousel-item">
                 <img src="gambar/Lada1.jpeg" class="col-block" alt="...">
-                  <div class="card-img-overlay col-none col-sm-block">
+                  <div class="card-img-overlay col-none col-md-block">
                     <div class="text2">
                     <ul class="list-unstyled text-end">
                       <li>We committed to the authenticity and </li>
@@ -104,24 +104,24 @@
   <div class="featur">
     <div class="container">
       <div class="row">
-        <div class="col-sm-3 text-center">
+        <div class="col-md-3 text-center">
           <img src="gambar/comitmen.png" alt="" class="card-img-top">
           <h5 class="card-title text-center">Commitment</h5>
               <p class="card-text">Commitment is important where we will continue to provide the best to our customers and investors all the time. Our opportunity to serve should be viewed as a privilege that is not to be taken for granted.</p>
         </div>
       
-        <div class="col-sm-3 text-center">
+        <div class="col-md-3 text-center">
           <img src="gambar/love.png" alt="" class="card-img-top">
           <h5 class="card-title text-center">Integrity</h5>
               <p class="card-text">Integrity means telling the truth, keeping our word and treating others with fairness and respect. Integrity is one of our most cherished assets. It must not be compromised.</p>
        </div>
-        <div class="col-sm-3 text-center">
+        <div class="col-md-3 text-center">
           <img src="gambar/inovaton.png" alt="" class="card-img-top">
           <h5 class="card-title text-center">Inovation</h5>
               <p class="card-text">Quality is exhibited in many ways by selling and supporting products and services that delight customers, establishing a work environment , delivering financial results that meet investor expectations.</p>
         </div>
       
-        <div class="col-sm-3 text-center">
+        <div class="col-md-3 text-center">
           <img src="gambar/quality.png" alt="" class="card-img-top">
           <h5 class="card-title text-center">Quality</h5>
               <p class="card-text">Quality is exhibited in many ways by selling and supporting products and services that delight customers, establishing a work environment , delivering financial results that meet investor expectations.</p>
@@ -138,45 +138,48 @@
 <section class="about" id="about">
   <div class="container">
     <div class="row">
-      <div class="col-sm-12">
+      <div class="col-md-12">
         <h2>About Us</h2>
       </div>
     </div>
   
   <div class="row">
-    <div class="col-sm-6">
+    <div class="col-md-6">
       <h5>We process more than 1,500 hectares of coconut plantations, spread across several islands in Eastern Indonesia.</h5>
       <p>In the beginning, our company was engaged in manufacturing where we processed coconut derivative products, 
         namely coconut shells into coconut shell charcoal.And after that our company expanded and expanded into coconut plantations, 
         to process more derivative products</p>
     </div>
-    <div class="col-sm-6 text-center">
+    <div class="col-md-6 text-center">
       <img src="gambar/{{ $img; }}" alt="">
     </div>
   </div>
   </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3b5045" fill-opacity="1" d="M0,96L48,128C96,160,192,224,288,229.3C384,235,480,181,576,186.7C672,192,768,256,864,272C960,288,1056,256,1152,224C1248,192,1344,160,1392,144L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
 </section>
 {{-- About End --}}
 
 {{-- Product --}}
 <section class="product" id="product">
-  <div class="container">
     <div class="row">
-      <div class="col-sm-12">
+      <div class="container">
+      <div class="col-md-12">
         <h1>Product</h1>
       </div>
     </div>
+    
     <div class="row">
-      <div class="col-sm-5">
+      <div class="col-md-6 text-end">
         <h3>White Pepper</h3>
         <p>Coconut fiber, is a natural fiber extracted from the husk of coconut[1] and used in products such as floor mats, doormats, brushes and mattresses. Coir is the fibrous material found between the hard, internal shell and the outer coat of a coconut. Other uses of brown coir (made from ripe coconut) are in upholstery padding, sacking and horticulture. White coir, harvested from unripe coconuts, is used for making finer brushes, string, rope and fishing net</p>
       </div>
-      <div class="col-sm-7 text-center">
+      <div class="col-md-6 text-center">
         <img src="gambar/pepper1.jpg" alt="">
       </div>
     </div>
+  </div>
     <div class="row">
-      <div class="col-sm-2 col-sm-offsite-10 text-center">
+      <div class="col-md-2 col-md-offsite-10 text-center">
         <button type="button" class="btn btn-outline-secondary">Contact Us <img src=""></button>
       </div>
     </div>
@@ -188,32 +191,32 @@
 <section class="team" id="team">
   <div class="container">
      <div class="row">
-       <div class="col-sm-12 text-end">
+       <div class="col-md-12 text-end">
          <h1>Our Team</h1>
        </div>
      </div>
   </div>
   <div class="container-fluid">
      <div class="row">
-       <div class="col-sm-3 text-center">
+       <div class="col-md-3 text-center">
          <img src="gambar/inul.png" alt="" class="rounded-circle"/>
          <h4>Inul</h4>
          <h5>Founder</h5>
          <p>Our faith is the substance of our future. There is no big success without big sacrifice.</p>
        </div>
-       <div class="col-sm-3 text-center">
+       <div class="col-md-3 text-center">
         <img src="gambar/pep.png" alt="" class="rounded-circle">
         <h4>Pep</h4>
         <h5>Co-Founder</h5>
         <p>Our faith is the substance of our future. There is no big success without big sacrifice.</p>
       </div>
-      <div class="col-sm-3 text-center" >
+      <div class="col-md-3 text-center" >
         <img src="gambar/lanza.png" alt="" class="rounded-circle">
         <h4>Lanza</h4>
         <h5>Marketing</h5>
         <p>Our faith is the substance of our future. There is no big success without big sacrifice.</p>
       </div>
-      <div class="col-sm-3 text-center">
+      <div class="col-md-3 text-center">
         <img src="gambar/brew.png" alt="" class="rounded-circle">
         <h4>BrewTaqi</h4>
         <h5>Development</h5>
@@ -221,7 +224,7 @@
       </div>
      </div>
   </div>
-</section>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3b5045" fill-opacity="1" d="M0,32L80,37.3C160,43,320,53,480,96C640,139,800,213,960,218.7C1120,224,1280,160,1360,128L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg></section>
 {{-- Team And --}}
 
 {{-- Contact --}}
@@ -229,18 +232,18 @@
   <div class="container-fluid">
   <div class="row">
     <div class="latar">
-    <div class="col-sm-12">
+    <div class="col-md-12 text-center">
       <h1>Contact</h1>
     </div>
     </div>
   </div>
     <div class="row">
-      <div class="col-sm-6">
+      <div class="col-md-6">
         <div class="text3">
         <h1>Did You Know White Pepper</h1>
         </div>
       </div>
-      <div class="col-sm-6 text-center">
+      <div class="col-md-6 text-center">
         <script src="http://maps.googleapis.com/maps/api/js"></script>
         <script>
           // fungsi initialize untuk mempersiapkan peta
@@ -260,7 +263,7 @@
       </div>
     </div>
       <div class="row">
-        <div class="col-sm-9 col-sm-offsite-3 text-center">
+        <div class="col-md-9 col-md-offsite-3 text-center">
           <img src="gambar/icon_ig.png" href="">
         </div>
       </div>
@@ -269,19 +272,18 @@
         
 {{-- Footer --}}
 <footer>
-  <div class="row">
-      <div class="col-sm-2 text-center">
-           <h1>Belanje</h1>
-            <p>Lada</p>
+  <div class="row" >
+      <div class="col-md-2 text-center">
+           <h4>Muntok <p>White Pepper</p> </h4>
       </div>
-        <div class="col-sm-6 text-center">
-          <h1>IG</h1>
+        <div class="col-md-6 text-center">
+          <h2>IG</h2>
             <h2>Faceebook</h2>
         </div>
-          <div class="col-sm-4 text-center">
-            <h5>Gg Denpasar XII No 190</>
-            <h5>abdullahtaqimahfuz@gmail.com</h5>
-            <h5>+62 813 9552 2636</h5>
+          <div class="col-md-4 text-center">
+            <h3>Gg Denpasar XII No 190</h3>
+            <h3>abdullahtaqimahfuz@gmail.com</h3>
+            <h3>+62 813 9552 2636</h3>
           </div>
     </div>
 </footer>
@@ -290,7 +292,6 @@
     
 <script src="js/jquery-3.6.0.min.js"> </script>
 <script src="js/bootstrap.min.js"></script>
-<script src="js/scritp.js"></script>
 
   </body>
 </html>
