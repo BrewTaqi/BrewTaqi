254af163857bdfc168a3c0727c5bf437ea2e875f
BrewTaqi <abdullahtaqimahfuz@gmail.com>
2021-12-31 15:04:35 +0700

    merombak team

--- expor_peper-laravel/public/js/scritp.js
+++ expor_peper-laravel/public/js/scritp.js
@@ -17,3 +17,19 @@ function hide_loading() {
         }
     }, 100);
 }
+
+// var swiper = new Swiper(".team .team-slide", {
+//     loop: true,
+//     direction: "vertical",
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//         rotate: 0,
+//         stretch: 0,
+//         depth: 0,
+//         modifier: 1,
+//         slideShadows: true,
+//     },
+// });
